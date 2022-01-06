d89dd54364477d13008059c2e9dfe9128e6804d3
BoWeii <blue85911360@gmail.com>
2022-01-06 18:01:08 +0800

    add block and cache

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -9,6 +9,7 @@ import d2ltvm
 tgt = tvm.target.Target(target="llvm", host="llvm")
 dev = tvm.device(tgt.kind.name, 0)
 
+
 def plot(X, Y, xlabel=None, ylabel=None, legend=[], xlim=None,
          ylim=None, xscale='linear', yscale='linear', fmts=None,
          figsize=(4.5, 3)):
@@ -34,11 +35,12 @@ def plot(X, Y, xlabel=None, ylabel=None, legend=[], xlim=None,
     # plt.plot()
     plt.show()
 
+
 # Save to the d2ltvm package
 def plot_gflops(sizes, gflops, legend, xlabel='Size'):
     plot(sizes, gflops, xlabel=xlabel, ylabel='GFLOPS',
-             xscale='log', yscale='log',
-             legend=legend, fmts=['--']*(len(gflops)-1)+['-'])
+         xscale='log', yscale='log',
+         legend=legend, fmts=['--'] * (len(gflops) - 1) + ['-'])
 
 
 # Save to the d2ltvm package.
@@ -52,9 +54,10 @@ def matmul(n, m, l):
     A = te.placeholder((n, l), name='A')
     B = te.placeholder((l, m), name='B')
     C = te.compute((n, m),
-                    lambda x, y: te.sum(A[x, k] * B[k, y], axis=k),
-                    name='C')
+                   lambda x, y: te.sum(A[x, k] * B[k, y], axis=k),
+                   name='C')
     return A, B, C
+
 
 # Save to the d2ltvm package.
 def get_abc(shape, constructor=None):
@@ -82,40 +85,47 @@ def bench_workload(workload):
     num_repeats = max(int(1.0 / time), 5)
     return workload(num_repeats) / num_repeats
 
+
 # Save to the d2ltvm package.
 def np_matmul_timer(n):
     timer = timeit.Timer(setup='import numpy as np\n'
-                         'import d2ltvm\n'
-                         'a, b, c = d2ltvm.get_abc(%s)' % str((n,n)),
-                         stmt = 'np.dot(a, b, out=c)')
+                               'import d2ltvm\n'
+                               'a, b, c = d2ltvm.get_abc(%s)' % str((n, n)),
+                         stmt='np.dot(a, b, out=c)')
     return timer.timeit
 
-sizes = 2**np.arange(5, 12, 1)
+
+sizes = 2 ** np.arange(5, 12, 1)
 exe_times = [bench_workload(np_matmul_timer(n)) for n in sizes]
-np_gflops = 2 * sizes **3 / 1e9 / np.array(exe_times)
+np_gflops = 2 * sizes ** 3 / 1e9 / np.array(exe_times)
+
 
 # Save to the d2ltvm package.
 def bench_matmul_tvm(func, sizes):
     def workload(nrepeats):
-        timer = mod.time_evaluator(mod.entry_name,dev,number=nrepeats)
+        timer = mod.time_evaluator(mod.entry_name, dev, number=nrepeats)
         return timer(a, b, c).mean * nrepeats
+
     times = []
     for n in sizes:
         s, (A, B, C) = func(int(n))
         mod = tvm.build(s, [A, B, C], tgt)
         a, b, c = get_abc((n, n), lambda x: tvm.nd.array(x, dev))
         times.append(bench_workload(workload))
-    return 2 * sizes**3 / 1e9 / np.array(times)
+    return 2 * sizes ** 3 / 1e9 / np.array(times)
+
 
 def default(n):
     A, B, C = matmul(n, n, n)
     return te.create_schedule(C.op), (A, B, C)
 
+
 # default part
-s,args=default(64)
+s, args = default(64)
 mod = tvm.build(s, args)
 print(tvm.lower(s, args, simple_mode=True))
 default_gflops = bench_matmul_tvm(default, sizes)
+
 
 def reorder(n):
     s, (A, B, C) = default(n)
@@ -130,10 +140,12 @@ mod = tvm.build(s, args)
 print(tvm.lower(s, args, simple_mode=True))
 reorder_gflops = bench_matmul_tvm(reorder, sizes)
 
+
 def parallel(n):
     s, (A, B, C) = reorder(n)
     s[C].parallel(C.op.axis[0])
     return s, (A, B, C)
+
 
 # parallel part
 s, args = parallel(64)
@@ -141,8 +153,53 @@ print(tvm.lower(s, args, simple_mode=True))
 parallel_gflops = bench_matmul_tvm(parallel, sizes)
 
 
-# draw the result
-plot_gflops(sizes, [np_gflops, default_gflops,reorder_gflops,parallel_gflops], ['numpy', 'default','reorder','parallel'])
+def block(n):
+    tx, ty, tk = 32, 32, 4
+    s, (A, B, C) = parallel(n)
+    xo, yo, xi, yi = s[C].tile(*C.op.axis, tx, ty)
+    xy = s[C].fuse(xo, yo)
+    s[C].parallel(xy)
+    # Optimize the computation of each block
+    ko, ki = s[C].split(s[C].op.reduce_axis[0], factor=tk)
+    s[C].reorder(ko, xi, ki, yi)
+    s[C].vectorize(yi)
+    s[C].unroll(ki)
+    return s, (A, B, C)
 
+
+# block part
+s, args = block(64)
+print(tvm.lower(s, args, simple_mode=True))
+blocked_gflops = bench_matmul_tvm(block, sizes)
+
+
+def cached_block(n):
+    tx, ty, tk = 32, 32, 4
+    s, (A, B, C) = parallel(n)
+    CachedC = s.cache_write(C, 'local')
+    # Same as before, first tile by blocks, and then parallelize the
+    # computation of each block
+    xo, yo, xi, yi = s[C].tile(*C.op.axis, tx, ty)
+    xy = s[C].fuse(xo, yo)
+    s[C].parallel(xy)
+    # Use the write cache for the output of the xy axis, namely a block.
+    s[CachedC].compute_at(s[C], xy)
+    # Same as before to optimize the computation of a block .
+    xc, yc = s[CachedC].op.axis
+    ko, ki = s[CachedC].split(CachedC.op.reduce_axis[0], factor=tk)
+    s[CachedC].reorder(ko, xc, ki, yc)
+    s[CachedC].unroll(ki)
+    s[CachedC].vectorize(yc)
+    return s, (A, B, C)
+
+
+# cached_block part
+s, args = cached_block(64)
+print(tvm.lower(s, args, simple_mode=True))
+cached_gflops = bench_matmul_tvm(cached_block, sizes)
+
+# draw the result
+plot_gflops(sizes, [np_gflops, default_gflops, reorder_gflops, parallel_gflops, blocked_gflops, cached_gflops],
+            ['numpy', 'default', 'reorder', 'parallel', 'block', 'cache'])
 
 print("end!!!!")
